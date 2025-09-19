f2ec6550eef957ad738ab163b837eca6390f595f
saitejamandadi99 <saitejamandadi99>
2025-09-19 16:16:18 +0530

    orders model created

---
+++ backend/models/ordersModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose
+const OrderSchema = new Schema({
+    customerId:{type:Schema.Types.ObjectId, ref:'Customer', required:true},  //ref id
+    productId:{type:Schema.Types.ObjectId, ref:'Product', required:true}, //ref id 
+    quantity:{type:Number,required:true},
+    totalRevenue:{type:Number, required:true},
+    reportDate:{type:Date, required:true}, 
+}, {timestamps:true});
+
+module.exports = mongoose.model('Order', OrderSchema)
