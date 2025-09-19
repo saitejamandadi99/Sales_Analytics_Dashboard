380ec15e6243227ca44afde380e10edaf2553cd7
saitejamandadi99 <saitejamandadi99>
2025-09-19 12:40:15 +0530

    added the mongo connection into the server

--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,36 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const axios = require('axios'); 
+
 
 
 
 const app = express()
 app.use(express.json())
 app.use(cors())
+dotenv.config();
 
-app.get('/',(req , res)=>{
-    res.send('Backend is running in the server')
+const MONGO_URL = process.env.MONGO_URL
+mongoose.connect(MONGO_URL)
+.then(()=>{
+    console.log('MongoDb is connected')
 })
-
-app.get((err,req,res,next)=>{
-    res.send('Error',err.message)
+.catch((err)=>{
     console.log('Error',err.message)
 })
 
-PORT = process.env.PORT 
-app.listen((PORT)=>{
+
+app.get('/',(req , res)=>{
+    res.status(200).send('Backend is running in the server')
+})
+
+app.use((err,req,res,next)=>{
+    res.status(500).send('Error',err.message)
+    console.log('Error',err.message)
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT,()=>{
     console.log(` server is running in http://localhost:${PORT}`); 
 })
 
