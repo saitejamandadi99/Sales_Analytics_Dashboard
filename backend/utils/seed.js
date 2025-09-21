ce98356db375b94851d917e4284bb910a9ef1b03
saitejamandadi99 <saitejamandadi99>
2025-09-21 16:04:41 +0530

    added await to seedcustomer

--- backend/utils/seed.js
+++ backend/utils/seed.js
@@ -24,7 +24,7 @@ const seedCustomersData = async () =>{
 
     await Customer.insertMany(customersData); 
     console.log('customer data added')
-    mongoose.disconnect();
+    await mongoose.disconnect();
 }
 
 
