4fdc48f41db0501f171ff74a05cf48c8c504884b
saitejamandadi99 <saitejamandadi99>
2025-09-21 15:33:14 +0530

    added the fake data to the product

---
+++ backend/utils/productSeed.js
@@ -0,0 +1,27 @@
+const {faker} = require('@faker-js/faker')
+const mongoose = require('mongoose')
+const Product = require('../models/productModel')
+require('dotenv').config(); 
+
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log('Mongodb connect with product seed')
+    seedProductData();
+})
+.catch(console.error);
+
+const seedProductData = async () =>{
+    const productData = []
+    for(let i = 0; i<100;i++){
+        productData.push({
+            name:faker.commerce.productName(),
+            category: faker.helpers.arrayElement(['Fashion', 'Electrical', 'Food', 'Home', 'Sports', 'Toys']),
+            price:Number(faker.commerce.price())
+        })
+    }
+    await   Product.insertMany(productData)
+    console.log(productData)
+    console.log('Seeded the Product data')
+    mongoose.disconnect();
+}
