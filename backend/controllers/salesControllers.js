644d99ce1e251c8e6ef9bdeaaddec9aac20faf14
saitejamandadi99 <saitejamandadi99>
2025-09-21 16:27:53 +0530

    added the population refernce to the salescontroller to add the original document data

--- backend/controllers/salesControllers.js
+++ backend/controllers/salesControllers.js
@@ -2,7 +2,9 @@ const Sales = require('../models/salesModel')
 
 const getSalesData = async (req , res) =>{
     try {
-        const sales = await Sales.find({})
+        const sales = await Sales.find({}) //populate is like automatic join in collections 
+        .populate('customerId', 'name region') // refers to the customerId and fetches the original content and fills 
+        .populate('productId', 'name category price') //refers to productId and fetches and fills the original content.
         res.status(200).json({message:'Sales data fetched successfully', sales:sales})
     } catch (error) {
         res.status(500).json({message:error.message})
